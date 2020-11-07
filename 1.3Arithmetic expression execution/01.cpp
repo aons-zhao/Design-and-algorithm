997b3e1661e638a38768ec8e495d8e3a039136fd
aons-zhao <zzhao1213@gmail.com>
2020-11-07 15:58:24 +0800

    1.3

---
+++ 1.3Arithmetic expression execution/01.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//给定两个整数A和B，输出A+B的值。保证A、B及结果均在整型范围内。
+int main()
+{
+	int a, b;
+	scanf("%d%d",&a, &b);
+	printf("%d",a+b);
+	return 0;
+}
---
+++ 1.3Arithmetic expression execution/02.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//给定3个整数a、b、c，计算表达式(a+b)*c的值。
+int main()
+{
+	int a, b, c;
+	scanf("%d%d%d",&a, &b, &c);
+	printf("%d",(a+b)*c);
+	return 0;
+}
